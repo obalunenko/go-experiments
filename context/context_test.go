9c739d329d71b5dc60bf7fa43e6f0ecc039fb3fa
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-04-12 15:05:32 +0400

    chore: Add context.WithCancelCause

--- context/context_test.go
+++ context/context_test.go
@@ -4,19 +4,38 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 )
 
-func job(ctx context.Context, cancel context.CancelCauseFunc) {
+func jobWithCause(ctx context.Context, cancel context.CancelCauseFunc) {
 	err := errors.New("test_error")
 
 	cancel(err)
 }
 
-func TestCancel(t *testing.T) {
+func TestCancelWithCause(t *testing.T) {
 	ctx := context.Background()
 	childCtx, cancel := context.WithCancelCause(ctx)
 
-	job(childCtx, cancel)
+	defer cancel(nil)
+
+	jobWithCause(childCtx, cancel)
+
+	t.Logf("parent err: %v", ctx.Err())
+	t.Logf("parent cause: %v", context.Cause(ctx))
+	t.Logf("child err: %v", childCtx.Err())
+	t.Logf("child cause: %v", context.Cause(childCtx))
+}
+
+func TestCancel(t *testing.T) {
+	ctx := context.Background()
+	childCtx, cancel := context.WithCancel(ctx)
+
+	childCtx, cancel = context.WithDeadline(childCtx, time.Date(2022, 12, 12, 0, 0, 0, 0, time.UTC))
+
+	cancel()
+
+	time.Sleep(time.Second * 2)
 
 	t.Logf("parent err: %v", ctx.Err())
 	t.Logf("parent cause: %v", context.Cause(ctx))
