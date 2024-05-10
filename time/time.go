007426c92e86fcd28a973aaf08941b12d430d94b
Oleg Balunenko <o.balunenko@pin-up.tech>
2024-05-10 15:54:01 +0400

    feat: Add time experiments

--- time/time.go
+++ time/time.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	nowInLoc := time.Now().In(time.FixedZone("UTC+12", 12*60*60))
 	fmt.Println(nowInLoc)
 	fmt.Println(nowInLoc.Format(time.RFC3339))
@@ -17,4 +16,12 @@ func main() {
 	fmt.Println(now)
 	fmt.Println(now.Format(time.RFC3339))
 
+	t1 := time.Time{}
+	t2 := time.Time{}
+
+	if t1.Before(t2) {
+		fmt.Println("t1 is before t2")
+	} else {
+		fmt.Println("t1 is not before t2")
+	}
 }
